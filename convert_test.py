bbef6c7a06485c46213b33d240ac61f5c9c20f91
akashch1512 <akashchaudhari1012@gmail.com>
2025-09-14 15:29:18 +0530

    final commit

--- convert_test.py
+++
@@ -1,9 +0,0 @@
-from Tools.image_overlay import convert_png_to_pdf
-import os
-png='data/generated/test_cert.png'
-pdf='data/generated/test_cert.pdf'
-if os.path.isfile(png):
-    p=convert_png_to_pdf(png,pdf)
-    print('PDF_SAVED',p)
-else:
-    print('PNG_MISSING')
